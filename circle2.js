70bfc17b68003611fbf5c45f9be5bef5e3afc685
Elijah Insua <tmpvar@gmail.com>
2014-01-28 23:00:48 -0700

    base implementation + tests

--- circle2.js
+++ circle2.js
@@ -1,16 +1,99 @@
-var circle2 = function() {
-  
+if (typeof require !== 'undefined') {
+  var Vec2 = require('vec2');
 }
 
+// TODO:
+//  - 2 point circles
+//  - 3 point circles
+
+function Circle(position, radius, last) {
+  if (!(this instanceof Circle)) {
+    return new Circle(x, y);
+  }
+
+  this.position = position || new Vec2(0, 0);
+  this.radius(radius || 1);
+  
+  this.position.change(this.notify.bind(this));
+  this._watchers = [];
+}
+
+Circle.prototype._watchers = [];
+Circle.prototype._radius = 1;
+Circle.prototype._radiusSquared = 1;
+
+Circle.prototype.isCircle = true;
+Circle.prototype.position = null;
+
+Circle.prototype.radius = function(radius) {
+  if (typeof radius !== 'undefined') {
+    this._radius = Vec2.clean(radius);
+    this._radiusSquared = radius*radius;
+    this.notify();
+  }
+
+  return this._radius;
+};
+
+Circle.prototype.notify = function() {
+  var w = this._watchers, l = w.length;
+  for (var i=0; i<l; i++) {
+    w[i](this);
+  }
+};
+
+Circle.prototype.change = function(fn) {
+  this._watchers.push(fn);
+  return fn;
+};
+
+Circle.prototype.ignore = function(fn) {
+  if (!fn) {
+    this._watchers = [];
+  } else {
+    this._watchers = this._watchers.filter(function(a) {
+      return a!==fn;
+    });
+  }
+  return this;
+};
+
+Circle.prototype.containsPoint = function(vec) {
+  return this.position.subtract(vec, true).lengthSquared() <= this._radiusSquared;
+};
+
+Circle.prototype.contains = function(thing) {
+
+  // Other circles
+  if (typeof thing.radius === 'function' && thing.position) {
+    if (thing.radius() < this._radius && this.containsPoint(thing.position)) {
+      var radius = thing.radius();
+      var distance =  thing.position.subtract(this.position).lengthSquared();
+      return distance + radius*radius <= this._radiusSquared;
+    }
+  } else if (typeof thing.points === 'function') {
+    var points = thing.points(), l = points.length;
+
+    for (var i=0; i<l; i++) {
+      if (!this.containsPoint(points[i])) {
+        return false;
+      }
+    }
+
+    return true;
+  }
+
+  return false;
+};
+
+Circle.prototype.containedBy = function(thing) {
+
+};
 
 if (typeof module !== "undefined" && typeof module.exports == "object") {
-  module.exports = circle2;
+  module.exports = Circle;
 }
 
 if (typeof window !== "undefined") {
-  window.circle2 = window.circle2 || circle2;
-}
-
-module.exports = function() {
-
+  window.Circle = window.Circle || Circle;
 }
--- test/test.js
+++ test/test.js
@@ -1,16 +1,177 @@
-var circle2;
+var Circle, Vec2;
 if (typeof require !== "undefined") {
-  circle2 = require("../circle2.js");
+  Circle = require("../circle2.js");
+  Vec2 = require("vec2");
 } else {
-  circle2 = window.circle2;
+  Circle = window.Circle;
+  Vec2 = window.Vec2;
 }
 
 var ok = function(a, msg) { if (!a) throw new Error(msg || "not ok"); };
 
 describe('circle2', function() {
-  describe('#', function() {
-    it('', function() {
-      
+  describe('ctor', function() {
+    it('should take a point and radius', function() {
+      var circle = new Circle(Vec2(0, 0), 10);
+      ok(circle.radius() === 10);
     });
+
+    it('should have sane defaults', function() {
+      var circle = new Circle();
+      ok(circle.position.equal(Vec2(0, 0)));
+      ok(circle.radius() === 1);
+    });
+
+    it('should create a Circle without the new keyword', function() {
+      var circle = Circle(Vec2(1, 1));
+      ok(circle.position.equal(Vec2(1, 1)));
+    });
+
+    // TODO: 2 and 3 point circles
+  });
+
+  describe('#change', function() {
+    it('should notify listeners', function(t) {
+      var c = new Circle();
+      c.change(function(obj) {
+        ok(obj === c);
+        t();
+      });
+      c.notify();
+    });
+
+    it('should return the passed function', function() {
+      var c = new Circle();
+      ok(true === c.change(true));
+    });
+
+  });
+
+  describe('#ignore', function() {
+    it('should remove a listener', function() {
+      var c = new Circle(), called = false;
+
+      var fn = function(obj) {
+        called = true;
+      };
+
+      c.change(fn);
+      c.ignore(fn);
+      c.notify();
+
+      ok(!called);
+    });
+
+    it('should remove all listeners when a fn is not passed', function() {
+      var c = new Circle(), called = false;
+
+      var fn = function(obj) {
+        called = true;
+      };
+
+      c.change(fn);
+      c.ignore();
+      c.notify();
+
+      ok(!called);
+    });
+  });
+
+  describe('#radius', function() {
+    it('should act as a getter and a setter', function() {
+      var circle  = new Circle();
+
+      circle.radius(10);
+      ok(circle.radius() === 10);
+
+      circle.radius(5.45);
+      ok(circle.radius() === 5.45);
+    });
+
+    it('should notify when changed', function(t) {
+      var circle  = new Circle();
+      circle.change(function() {
+        t();
+      });
+
+      circle.radius(10);
+    });
+
+    it('should clean incoming values', function() {
+      var circle  = new Circle();
+      circle.radius(0.1 + 0.2);
+      ok(circle.radius() === 0.3);
+    });
+  });
+
+  describe('#containsPoint', function() {
+    it('should return true if passed point is inside', function() {
+      var circle  = new Circle(Vec2(0, 0), 10);
+
+      ok(circle.containsPoint(Vec2(0, 0)));
+    });
+
+    it('should return true if passed point is on the boundary', function() {
+      var circle  = new Circle(Vec2(0, 0), 10);
+      ok(circle.containsPoint(Vec2(10, 0)));
+    });
+
+    it('should return false if passed point is outside', function() {
+      var circle  = new Circle(Vec2(0, 0), 10);
+      ok(!circle.containsPoint(Vec2(10, 10)));
+    });
+  });
+
+  describe('#contains', function() {
+    it('should detect concentric circles', function() {
+      var c  = new Circle(Vec2(0, 0), 10);
+      var c2  = new Circle(Vec2(0, 0), 11);
+
+      ok(!c.contains(c2));
+      ok(c2.contains(c));
+    });
+
+    it('should detect offset circles', function() {
+      var c  = new Circle(Vec2(6, 0), 5);
+      var c2  = new Circle(Vec2(0, 0), 11);
+
+      ok(!c.contains(c2));
+      ok(c2.contains(c));
+    });
+
+
+    it('should handle polygon-like things', function() {
+
+      var p = {
+        points : function() {
+          return [
+            Vec2(10, 0),
+            Vec2(-10, 0),
+            Vec2(0, 10),
+            Vec2(0, -10)
+          ];
+        }
+      };
+
+      var c  = new Circle(Vec2(0, 0), 10);
+      ok(c.contains(p));
+    });
+
+    it('should handle polygon-like things', function() {
+      var p = {
+        points : function() {
+          return [
+            Vec2(10, 0),
+            Vec2(-11, 0),
+            Vec2(0, 10),
+            Vec2(0, -10)
+          ];
+        }
+      };
+
+      var c  = new Circle(Vec2(0, 0), 10);
+      ok(!c.contains(p));
+    });
+
   });
 });
